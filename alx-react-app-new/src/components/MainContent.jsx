67ab244a9914b78a797c7bbd9f8db2867c7c3516
Usman Shittu <usmanola1995@gmail.com>
2025-07-19 23:00:53 +0100

    Applied Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+            <main style={{ backgroundColor: 'purple', color: 'black', textAlign: 'center' }}>
+             <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+    );
+}
+
+export default MainContent;
