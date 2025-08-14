3a92fe36c2e4837b0f6197ab80b5f56f9f6535b8
Usman Shittu <usmanola1995@gmail.com>
2025-08-14 22:13:22 +0100

    fixed a bug

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage'
 import RecipeDetail from './components/RecipeDetail'
 
